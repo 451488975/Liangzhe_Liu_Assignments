2536b74bdef9a70271d6dcbca0b65a5c250d9c34
451488975 <451488975@qq.com>
2023-10-02 13:19:43 -0500

    Add session 15 assignments

---
+++ Session_15_Assignment/BackgoundThreadsDemo.cs
@@ -0,0 +1,49 @@
+﻿// Please change the starting project in the solution to this project
+// This program demostrate the use of background threads
+// Background threads are terminated when the main thread is terminated
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BackgoundThreadsDemo
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main Thread Started");
+            Thread t1 = new Thread(Count);
+            Thread t2 = new Thread(BackgroundCount);
+            t2.IsBackground = true;
+            t1.Start();
+            t2.Start();
+            Console.WriteLine("Main Thread Completed");
+        }
+
+        private static void Count()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Count: " + i);
+                Thread.Sleep(500);
+                // uncomment the following line to see the difference
+                //Thread.Sleep(501);
+                if (i == 10)
+                    Console.WriteLine("Count Completed");
+            }
+        }
+        private static void BackgroundCount()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Background: " + i);
+                Thread.Sleep(1000);
+                if (i == 10)
+                    Console.WriteLine("Background Count Completed");
+            }
+        }
+    }
+}
---
+++ Session_15_Assignment/ThreadPoolDemo.cs
@@ -0,0 +1,35 @@
+﻿// Please change the starting project in the solution to this project
+// This program demostrate the use of thread pool
+// Thread pool threads are background threads, so they are terminated when the main thread is terminated
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ThreadPoolDemo
+    {
+        public static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(Count);
+            Console.WriteLine("Main Thread Started");
+            Thread.Sleep(1000);
+            // uncomment the following line to see the difference
+            //Thread.Sleep(5000);
+            Console.WriteLine("Main Thread Completed");
+        }
+
+        private static void Count(object state)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Count: " + i);
+                Thread.Sleep(500);
+                if (i == 10)
+                    Console.WriteLine("Count Completed");
+            }
+        }
+    }
+}
